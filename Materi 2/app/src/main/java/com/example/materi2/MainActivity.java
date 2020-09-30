0ea30ba9a8cb9f6ad6f57d765f56c186aea5c934
Gunawan Prasetya <gunawan29092000@gmail.com>
2020-09-30 21:54:03 +0700

    Final Commid

---
+++ Materi 2/app/src/main/java/com/example/materi2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.materi2;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+        import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Google Pixel");
+        }
+    }
+
+
+}
---
+++ Materi 2/app/src/main/java/com/example/materi2/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.materi2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private int waktu_loading=4000;
+
+    //4000=4 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+}
